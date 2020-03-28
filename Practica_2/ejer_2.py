fb3c1b64f31bcdc269a0d77d9e25a8382c0464b8
luciomolina365 <lucio_molina_365@live.com>
2020-03-27 21:05:53 -0300

    ejer2 y mod

---
+++ Practica_2/ejer_2.py
@@ -0,0 +1,14 @@
+tam = ['im1 4,14', 'im2 33,15', 'im3 6,34', 'im4 410,134']
+
+nuevo = []
+
+for i in tam:
+    name, space, tuplaStr = i.partition(" ")
+
+    arreglo=(int(tuplaStr.split(",")[0]),int(tuplaStr.split(",")[1]))
+    print(arreglo)
+
+    nuevo.extend([arreglo])
+
+nuevo.sort()
+print(nuevo)
