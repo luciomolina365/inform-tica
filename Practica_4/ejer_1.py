a3f11e19c7105c5f4b11db8a1af8d4e7d09fbc93
luciomolina365 <lucio_molina_365@live.com>
2020-05-08 22:25:58 -0300

    ahora si ejer_1 P4

--- Practica_4/ejer_1.py
+++ Practica_4/ejer_1.py
@@ -3,8 +3,6 @@ import time
 import json
 
 #--------------------------------------------------------------------------
-
-
 def leerArchivo():
     # Tarea: Pensar como manejar exceptions aca
     try:
@@ -27,20 +25,19 @@ def escribirArchivo(clima):
 def cargarJugador(lista):
 
     datos=leerArchivo()
-
-    datos[len(datos)] = {
-        "Temperatura": lista[0],
-        "Humedad": lista[1],
-        "Fecha": lista[2]
-    }
-
+    
+    for elemento in lista:
+        
+        datos[len(datos)+1]= elemento
+    
+    
     escribirArchivo(datos)
-
-
+    
 #--------------------------------------------------------------------------
 
+#PROGRAMA PRINCIPAL
 
-
+#Definimos la ventana
 layout = [
     [sg.Text("Temperatura") , sg.Input(key='_temperatura_')],
     [sg.Text('Humedad'),sg.Input(key='_humedad_')],    
@@ -48,13 +45,12 @@ layout = [
     [sg.Button('Guardar en json'), sg.Exit()]
 ]
 
-
 window = sg.Window('Ejercicio 1').Layout(layout).Finalize() 
 # Loop para capturar los eventos de window
+lista=[]
 while True:
     event, values = window.Read()
     
-
     if event is None or event == 'Exit':      
         break
 
@@ -65,9 +61,17 @@ while True:
             
         else:         
             fecha= time.strftime("%d %b %Y , %H:%M", time.localtime())
-            lista = [str(values['_temperatura_']) + " grados" , str(values['_humedad_']) + "%" , fecha]
+            aux = {
+                "Temperatura": str(values['_temperatura_']) + " grados" , 
+                "Humedad": str(values['_humedad_']) + "%" , 
+                "Fecha": fecha
+            }
+
+            lista.append(aux)
     
     elif event == "Guardar en json":
-        try:cargarJugador(lista)
+        try:
+            cargarJugador(lista)
+            lista = []
         except NameError:
             sg.Popup('Falta añadir los datos')
