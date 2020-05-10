ce323550b053b2822824a3946ddefcba4a3bb333
luciomolina365 <lucio_molina_365@live.com>
2020-05-10 03:32:37 -0300

    ejer_3 practica 4

--- Practica_4/ejer_3.py
+++ Practica_4/ejer_3.py
@@ -1,32 +1,73 @@
-import json
 
-def leerArchivo():
-    # Tarea: Pensar como manejar exceptions aca
+import json
+import pattern
+import pattern.text.es as ptt
+from pattern.text.es import conjugate , INFINITIVE
+
+
+def leerTxt():
     try:
-        with open('verbos.json', 'r') as archivo:
+        with open('texto.txt', 'r') as archivo:
             datos = json.load(archivo)
         return datos  
     
     except FileNotFoundError:
-        with open('verbos.json', 'w') as archivo:
-            datos={}
+        with open('texto.txt', 'w') as archivo:
+            datos="Queen es una banda británica de rock formada en 1970 en Londres por el cantante Freddie Mercury, el guitarrista Brian May, el baterista Roger Taylor y el bajista John Deacon. Si bien el grupo ha presentado bajas de dos de sus miembros (Mercury, fallecido en 1991, y Deacon, retirado en 1997), los integrantes restantes, May y Taylor, continúan trabajando bajo el nombre Queen, por lo que la banda aún se considera activa."
             json.dump(datos, archivo)        
-        return datos
+        return datos   
 
-def escribirArchivo(datos):
+
+def txt_a_lista():
+    texto = leerTxt()
+    resultado = texto.split()
+    return resultado
+
+def esVerbo(palabra):
+    palabra = conjugate(palabra, INFINITIVE)
+    encuentro = False
+    if palabra in ptt.verbs.keys():
+        encuentro = True
+
+    if encuentro:
+        return palabra
+    else:
+        return ""
+
+def crearListaDeVerbos(lista):
+    listaResultado=[]
+    for palabra in lista:        
+        palabra = esVerbo(palabra)
+        if palabra != "":
+            listaResultado.append(palabra)
+    
+    
+    return listaResultado
+
+
+def txt_a_lista_de_verbos():
+    lista = txt_a_lista()
+    return crearListaDeVerbos(lista)
+
+
+
+def crearJson(lista):
+    datos={}
+
+    for verbo in lista:
+        
+        if verbo not in datos:
+            datos[verbo]= lista.count(verbo)
+
     with open('verbos.json' , 'w') as archivo:
         json.dump(datos,archivo)
 
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-def cargarDatos(values):
 
-    jugadores = leerArchivo()
-    
-    nombre = str(values['_nombre_']).lower()
-    jugadores[nombre] = {
-        'puntaje': int(values['_puntajemax_']),
-        'nivel': int(values['_nivel_']),
-        'tiempo': int(values['_tiempo_'])
-    }
 
-    escribirArchivo(jugadores)
+resultado = txt_a_lista_de_verbos()
+
+print(resultado)
+
+crearJson(resultado)
--- Practica_4/kkkk.py
+++ Practica_4/kkkk.py
@@ -5,3 +5,13 @@ datos = [{'nombre': 'Tony Stark', 'fecha': time.strftime("%a, %d %b %Y %H:%M:%S 
  {'nombre': 'Bruce Wayne', 'fecha': time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())}]
 json.dump(datos, archivo)
 archivo.close()
+
+
+
+
+
+lista = ["hola", "hola"]
+
+cant = lista.count("hola")
+
+print(cant)
